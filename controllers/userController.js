0161868d0f28c69ee4f6a10136aea129d56ffbba
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-09-21 00:23:44 +0500

    resolved errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,54 +1,56 @@
-const User=require('../models/userModel')
+const User = require("../models/userModel");
 
-const test=async(req, res)=>{
+const test = async (req, res) => {
+  try {
+    res.status(200).send({ success: true, msg: "API working" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-    try{
+const login = async (req, res) => {
+  console.log(`Login API hit`, req.body);
+  try {
+    const { email, password } = req.body;
 
-        res.status(200).send({success:true, msg:"API working"})
-    }catch(error){
-        res.status(400).send(error)
+    const user = await User.findOne({ email, password });
+
+    if (user) {
+      res
+        .status(200)
+        .send({ success: true, msg: "Loggedin successfully", data: user });
+    } else {
+      res.status(200).send({ success: false, msg: "Could not find user" });
     }
-}
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-const login=async(req, res)=>{
-
-    console.log(`Login API hit`, req.body)
-    try{
-        const {email, password}=req.body
-
-        const user=await User.findOne({email, password})
-
-        if(user){
-            res.status(200).send({success:true, msg:"Loggedin successfully", data:user})
-        }
-        else{
-            res.status(200).send({success:false, msg:"Could not find user"})
-        }
-    }catch(error){
-        res.status(400).send(error)
+const register = async (req, res) => {
+  console.log(`Register API hit`);
+  try {
+    const { email, password, publicAddress, privateAddress } = req.body;
+    const user = await User.create({
+      email: email,
+      password: password,
+      private_address: privateAddress,
+      public_address: publicAddress,
+    });
+    if (user) {
+      res
+        .status(200)
+        .send({ success: true, msg: "Created successfully", data: user });
+    } else {
+      res.status(200).send({ success: false, msg: "Could not Create" });
     }
-}
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-const register=async (req, res)=>{
-
-    console.log(`Login API hit`)
-    try{
-        
-
-        const user=await User.create({...req.body})
-        if(user){
-            res.status(200).send({success:true, msg:"Created successfully", data:user})
-        }
-        else{
-            res.status(200).send({success:false, msg:"Could not Create"})
-        }
-    }catch(error){
-        res.status(400).send(error)
-    }
-}
-
-module.exports={
-    login,
-    register,
-    test
-}
+module.exports = {
+  login,
+  register,
+  test,
+};
